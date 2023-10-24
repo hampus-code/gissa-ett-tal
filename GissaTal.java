30a5dd5a1c335ce05085d9812d98286114bd352c
hampus-code <hampusss96@gmail.com>
2023-10-24 14:56:27 +0200

    Added a loop if user guess a number over 100 + I changed some text

--- GissaTal.java
+++ GissaTal.java
@@ -4,25 +4,24 @@ import java.util.Random;
 
 public class GissaTal {
 
-    Random randomNumber = new Random();
-    int userGuessedNumber;
-    int rightNumber;
-    Scanner scanner;
-    int counter;
-    boolean gameRunner;
+    private Random randomNumber = new Random();
+    private Scanner scanner = new Scanner(System.in);
+    
+    private int userGuessedNumber;
+    private int rightNumber;
+    private int counter;
+    private boolean gameRunner;
 
     public GissaTal(){
         
        runGame();
        
-        
     }
-
 
     public void runGame(){
 
         gameRunner = true;
-        scanner = new Scanner(System.in);
+        
         rightNumber = randomNumber.nextInt(100) + 1;
 
         System.out.println("Gissa på ett tal mellan 1 och 100.");
@@ -30,29 +29,37 @@ public class GissaTal {
 
         do{
             
-
-             counter++;
+            counter++;
             System.out.print("Gissning " + counter + ": ");
-             try{
-            userGuessedNumber = scanner.nextInt();
-            //checkInput(userGuessedNumber);
 
-            if (userGuessedNumber == rightNumber){
-                System.out.print("Talet är " + rightNumber + 
-                " och du gissade rätt, på " + counter + " försök\n");
+            try{
+
+            userGuessedNumber = scanner.nextInt();
+
+            if (userGuessedNumber > 100){
+                counter --;
+                System.out.println("Du måste ange ett tal mellan 1 och 100! Försök igen.");
+            }
+
+            else if (userGuessedNumber == rightNumber){
+                System.out.print("Rätt! Talet är " + rightNumber + 
+                " och du gissade rätt på " + counter + " försök.\n");
                 restartGameOrNot();
                 
             }
+
             else if (userGuessedNumber > rightNumber){
                 System.out.println("Talet är mindre, gissa igen!");
             }
+
             else if (userGuessedNumber < rightNumber){
                 System.out.println("Talet är större, gissa igen!");
             }
-              }
+
+            }
              catch (InputMismatchException e){
                 counter --;
-                 System.out.println("Felaktig inmatning, du måste ange tal i siffror! Försök igen!");
+                 System.out.println("Felaktig inmatning, du måste ange ett tal i siffror! Försök igen!");
                  scanner.next();
              }
         
@@ -63,44 +70,27 @@ public class GissaTal {
 
     public void restartGameOrNot(){
 
-        System.out.println("Vill du spela igen (J/N)?");
+        System.out.println("Vill du spela igen (Ja/Nej)?");
         String continueOrNot = scanner.next();
 
-        if (continueOrNot.equalsIgnoreCase("J")){
+        if (continueOrNot.equalsIgnoreCase("Ja")){
             counter = 0;
             runGame();
 
               
         }
-        else if (continueOrNot.equalsIgnoreCase("N")){
+        else if (continueOrNot.equalsIgnoreCase("Nej")){
             
-            System.out.println("Programmet avslutas, tack för den här gången");
+            System.out.println("Programmet avslutas, tack för den här gången!");
             gameRunner = false;
-            
-                
+                   
         }
         else {
-            System.out.println("Du måste ange eller J/N!");
+            System.out.println("Du måste ange Ja eller Nej!");
             restartGameOrNot();
         }
 
-        
     }
-
-/*     public void checkInput(int userGuessedNumber){
-
-    try{
-        
-            System.out.print("Gissning " + counter + ": ");
-            // try{
-            userGuessedNumber = scanner.nextInt();
-
-    }
-    catch (InputMismatchException e){
-    System.out.println("Felaktig inmatning!");
-    scanner.next();
-    }
-    } */
 
             
 }
